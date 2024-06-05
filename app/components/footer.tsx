ffebf571d48e133047465b9a52e27039c4c1be11
Samuel <samuelint@gmail.com>
2024-06-05 10:07:01 -0400

    fix: code source broken link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/samuelint/portfolio"
           >
             <ArrowUpRightIcon className="size-4" />
             <p className="ml-2 h-7">view source</p>
